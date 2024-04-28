a8016db25dec153f49de641e25e94477302649b5
Matvey <114231790+Flozeen@users.noreply.github.com>
2024-04-28 12:10:48 +0300

    add template

---
+++ js4/js/main.js
@@ -0,0 +1,15 @@
+let age = 17;
+let message = (age >= 18) ? 'Вы совершеннолетний' : 'Вам еще нет 18';
+console.log(message);
+
+
+
+
+
+// let age = 25;
+
+// if (age >= 18) {
+//     console.log('Вы совершеннолетний');
+// } else {
+//     console.log('Вам еще нет 18');
+// }
