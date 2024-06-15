78996ae119d5089be000b1b3856c939dbd142c14
Matvey <114231790+Flozeen@users.noreply.github.com>
2024-06-15 12:31:39 +0300

    add template

---
+++ diplom/js/main.js
@@ -0,0 +1,9 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 150,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+});
