5408cdceeaabfd0f8b97464d80652b12170fec72
Matvey <114231790+Flozeen@users.noreply.github.com>
2024-06-18 10:29:08 +0300

    Delete diplom directory

--- diplom/js/main.js
+++
@@ -1,9 +0,0 @@
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 6,
-    spaceBetween: 150,
-    pagination: {
-        el: ".swiper-scrollbar",
-        type: "progressbar",
-    },
-});
